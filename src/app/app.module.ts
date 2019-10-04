0972586d950c32b4a9cb6c2254c1972a90ce8b06
Dare Lawal <lawaldare@gmail.com>
2019-10-04 22:10:41 +0100

    added folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { RegisterFormComponent } from './component/register-form/register-form.component';
+import { UserCardComponent } from './component/user-card/user-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    RegisterFormComponent,
+    UserCardComponent,
   ],
   imports: [
     BrowserModule,
