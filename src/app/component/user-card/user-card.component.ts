a43ae1b6db60c9cf0bdc37bbbbbe5cbfaa989aef
Busry <minitext@gmail.com>
2019-10-07 07:50:32 +0100

    Add card to dashboar 	Now the card takes input from the dashbord i.e profiles. using the ngfor="".The problem now is the imageUrl from the data is not been handle well and profile image not displaying

--- src/app/component/user-card/user-card.component.ts
+++ src/app/component/user-card/user-card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-user-card',
-  templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.css']
+  selector: "app-user-card",
+  templateUrl: "./user-card.component.html",
+  styleUrls: ["./user-card.component.css"]
 })
 export class UserCardComponent implements OnInit {
+  @Input() profile: any;
+  // = {
+  //   fullName: "Abdul Busari",
+  //   imageUrl: "/assets/woman.jpg",
+  //   desc: "i love to program using css js and html",
+  //   email: "minitext@gmail.com",
+  //   stack: "fullstack developer",
+  //   country: "Nigeria",
+  //   title: "full",
+  //   githubProfileUrl: "https://github.com/Busry"
+  // };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
