2469317dc08b677d8535e0b8830e38a543e4a21c
Dare Lawal <lawaldare@gmail.com>
2019-10-12 20:00:59 +0100

    did restructuring

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
+import { Profile } from 'src/app/model/user.model';
 import { Component, OnInit } from "@angular/core";
 import { UserService } from "src/app/service/user.service";
 import { ToastrService } from "ngx-toastr";
-import { UserCardComponent } from "../user-card/user-card.component";
+import { NgxSpinnerService } from 'ngx-spinner';
+
 
 @Component({
   selector: "app-dashboard",
@@ -10,10 +12,24 @@ import { UserCardComponent } from "../user-card/user-card.component";
 })
 export class DashboardComponent implements OnInit {
   profiles: any;
+  stackTerm: string;
+  filteredProfiles: any;
+  filteredProfiles1: any;
+  filteredProfiles2: any;
+
+  // set frontendTerm(stack) : string {
+  //   this.stackTerm = stack;
+  //   this.filteredProfiles = this.getFrontend(stack)
+  // }
+
+
+
+
 
   constructor(
     private userService: UserService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
   ) {}
 
   ngOnInit() {
@@ -21,14 +37,42 @@ export class DashboardComponent implements OnInit {
   }
 
   fetchProfiles() {
+    this.spinner.show();
     this.userService.onFetchProfiles().subscribe(
       data => {
-        this.profiles = data;
-        console.log(this.profiles);
+        setTimeout(() => {
+          this.profiles = data;
+          this.spinner.hide();
+        }, 1000);
       },
       error => {
         this.toastr.error("Unable to fetch Profiles");
+        this.spinner.hide();
+      });
       }
-    );
-  }
+
+      // frontend(){
+      //   this.filteredProfiles = this.profiles.filter(profile => {
+      //     return profile.stack === 'frontend';
+      //   })
+      //   this.profiles = this.filteredProfiles;
+      // }
+
+      // backend(){
+      //   this.filteredProfiles1 = () => this.profiles.filter(profile => {
+      //     return profile.stack === 'backend';
+      //   })
+      //   this.profiles = this.filteredProfiles1;
+      // }
+
+      // fullstack(){
+      //   this.filteredProfiles2 = this.profiles.filter(profile => {
+      //     return profile.stack === 'fullstack';
+      //   })
+      //   this.profiles = this.filteredProfiles2;
+      // }
+
+      // all(){
+      //   this.filteredProfiles = this.profiles;
+      //}
 }
--- src/app/component/user-card/user-card.component.ts
+++ src/app/component/user-card/user-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { Profile } from 'src/app/model/user.model';
 
 @Component({
   selector: "app-user-card",
@@ -6,19 +7,9 @@ import { Component, OnInit, Input } from "@angular/core";
   styleUrls: ["./user-card.component.css"]
 })
 export class UserCardComponent implements OnInit {
-  @Input() profile: any;
-  viewMode: boolean;
-  smallDesc: string;
+  @Input() profiles: Profile[];
   constructor() {}
   ngOnInit() {
-    if (this.profile.desc.length > 25) {
-      this.viewMode = true;
-      this.smallDesc = this.profile.desc.slice(0, 60);
-    } else {
-      this.viewMode = false;
-    }
   }
-  changeView() {
-    this.viewMode = !this.viewMode;
-  }
+
 }
